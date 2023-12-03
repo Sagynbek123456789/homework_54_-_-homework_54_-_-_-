00fc7e579abad9bdc22159c786478f6e16da8cd9
Sagynbek <jyldyzbekov-s@mail.ru>
2023-12-03 21:44:11 +0600

    создал файл URLS

---
+++ sours/webapp/urls.py
@@ -0,0 +1,9 @@
+# from django.urls import path
+# from webapp.views import index_view, task_create_view, task_delete_view, task_view
+#
+# urlpatterns = [
+#     path('', index_view),
+#     path('task/add/', task_create_view),
+#     path('task/delete/', task_delete_view),
+#     path('task/<int:pk>/', task_view)
+# ]
